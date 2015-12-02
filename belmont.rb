41eca170aba7024cf0cf3f9a9d5577bf16e4a57b
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2015-12-02 11:05:11 -0700

    I successfully created the moving animation

--- belmont.rb
+++ belmont.rb
@@ -1,7 +1,7 @@
 
 
 class Belmont
-	attr_reader :x, :y
+	attr_reader :x, :y, :backwards
 
 	def initialize(animation)
 		@animation = animation
@@ -9,8 +9,19 @@ class Belmont
 		@y = 20
 	end
 
-	def draw
-		img = @animation[Gosu::milliseconds / 100 % @animation.size];
+	def forward
+		@backwards = false
+		@x += 1
+	end
+
+	def backward
+		@backwards = true
+		@x -= 1
+
+	end
+
+	def draw(animation)
+		img = animation[Gosu::milliseconds / 150 % animation.size];
 		img.draw(@x, @y, 1)
 	end
 end
--- main.rb
+++ main.rb
@@ -3,17 +3,33 @@ require_relative "belmont"
 require 'gosu'
 
 class Castlevania < Gosu::Window
+	attr_reader :backwards
+
 	def initialize
 		super 600, 360
 		@background = Gosu::Image.new("background.png", :tileable => true)
-		@character_anim = Gosu::Image::load_tiles("cst.png", 20, 60)
+		@character_forward = Gosu::Image::load_tiles("cst.png", 20, 60)
+		@character_backward = Gosu::Image::load_tiles("cstback.png", 20, 60)
 		@belmont = Belmont.new(@character_anim)
+	end
+
+	def update
+
+		@belmont.forward && @backwards = false if Gosu::button_down? Gosu::KbRight
+		@belmont.backward && @backwards = true if Gosu::button_down? Gosu::KbLeft
 	end
 
 	def draw
 		@background.draw(0,0,0)
-		@belmont.draw
-		
+		if @backwards
+			@belmont.draw(@character_forward)	
+		else
+			@belmont.draw(@character_backward)
+		end
+	end
+
+	def button_down(id)
+		close if id == Gosu::KbEscape
 	end
 
 end
