20d1f11dbb979859e33d243db16c697e3020d3fe
Tannor Ziehm <ziehmtj@s.dcsdk12.org>
2015-12-02 10:18:35 -0700

    I have created the initial background image, as well as located a sprite

--- main.rb
+++ main.rb
@@ -1,8 +1,19 @@
-
+require 'gosu'
 class Castlevania < Gosu::Window
 	def initialize
-		super 1024, 1024
-		@background = Gosu::Image.new("background.jpg", :tileable => true)
+		super 600, 360
+		@background = Gosu::Image.new("background.png", :tileable => true)
+		@character = Gosu::Image.new("belmont.png", :tileable => true)
 	end
+
+	def draw
+		@background.draw(0,0,0)
+		@character.draw(10,10,1)
+	end
+
+end
+
+window = Castlevania.new
+window.show
 
 
