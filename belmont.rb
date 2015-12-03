280cc120b3b98dbd3246ecc95c90b7e597c3b0e3
Tannors Tablet <tziehm@comcast.net>
2015-12-02 22:35:06 -0700

    I added a standing sprite such that he isnt always moving

--- belmont.rb
+++ belmont.rb
@@ -1,27 +1,33 @@
 
 
 class Belmont
-	attr_reader :x, :y, :backwards
+	attr_reader :x, :y, :backwards, :standing
 
 	def initialize(animation)
 		@animation = animation
 		@x = 10
-		@y = 20
+		@y = 283
+		@standing = true
 	end
 
 	def forward
-		@backwards = false
+		# @backwards = false
 		@x += 1
+		# @standing = false
 	end
 
 	def backward
-		@backwards = true
+		# @backwards = true
 		@x -= 1
-
+		# @standing = false
 	end
 
-	def draw(animation)
+	def animate(animation)
 		img = animation[Gosu::milliseconds / 150 % animation.size];
 		img.draw(@x, @y, 1)
+	end
+
+	def draw(pic)
+		pic.draw(@x, @y, 1)
 	end
 end
--- main.rb
+++ main.rb
@@ -3,28 +3,40 @@ require_relative "belmont"
 require 'gosu'
 
 class Castlevania < Gosu::Window
-	attr_reader :backwards
+	attr_accessor :backwards, :standing
 
 	def initialize
 		super 600, 360
 		@background = Gosu::Image.new("background.png", :tileable => true)
+		@character_standing = Gosu::Image.new("belmont.png", :tileable => true)
 		@character_forward = Gosu::Image::load_tiles("cst.png", 20, 60)
 		@character_backward = Gosu::Image::load_tiles("cstback.png", 20, 60)
 		@belmont = Belmont.new(@character_anim)
+		@backwards = false
 	end
 
 	def update
-
-		@belmont.forward && @backwards = false if Gosu::button_down? Gosu::KbRight
-		@belmont.backward && @backwards = true if Gosu::button_down? Gosu::KbLeft
+		if Gosu::button_down? Gosu::KbRight
+			@belmont.forward
+			@backwards = false
+			@standing = false 
+		elsif Gosu::button_down? Gosu::KbLeft
+			@belmont.backward
+			@backwards = true
+			@standing = false
+		else
+			@standing = true
+		end
 	end
 
 	def draw
 		@background.draw(0,0,0)
-		if @backwards
-			@belmont.draw(@character_forward)	
+		if (@backwards ==  false && @standing == false)
+			@belmont.animate(@character_forward)
+		elsif (@backwards == true && @standing == false)
+			@belmont.animate(@character_backward)
 		else
-			@belmont.draw(@character_backward)
+			@belmont.draw(@character_standing)
 		end
 	end
 
